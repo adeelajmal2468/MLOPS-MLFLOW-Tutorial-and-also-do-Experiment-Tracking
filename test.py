4f875b38d971ff2d0f22d6b397c4ccb9ba847aec
Adeel <adeelajmal2468@gmail.com>
2025-10-18 18:27:37 +0500

    Updated till MLFLOW autolog

---
+++ test.py
@@ -0,0 +1,9 @@
+import mlflow
+print("Printing Tracking URI Scheme Below")
+print(mlflow.get_tracking_uri())
+print("\n")
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+print("Printing new Tracking URI Scheme Below")
+print(mlflow.get_tracking_uri())
+print("\n")
